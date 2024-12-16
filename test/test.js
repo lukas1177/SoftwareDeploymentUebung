db7e148bf5a667fa135e3caa054f0cdd33e0835e
cNiklos <134613088+cNiklos@users.noreply.github.com>
2024-12-16 17:40:36 +0100

    Added sample nodejs project

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+
+describe('Array', function () 
+{
+  describe('#indexOf()', function () 
+  {
+    it('should return 2', function () 
+    {
+      assert.equal(1 + 1, 2);
+    });
+  });
+});
